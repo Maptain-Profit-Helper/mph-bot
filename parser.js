393cb819c46ab7b3cc82204a9b6874c0e3b64610
topheryun <topheryun@gmail.com>
2021-02-08 14:51:38 -0800

    added logic to parseLine() and created function canSkipWord()

--- parser.js
+++ parser.js
@@ -8,8 +8,31 @@ module.exports = {
 }
 
 function parseLine(line) {
-    Mouse = { name: "", price: 0 };
-    // console.log(miceNames);
-    let test = miceNames.get("white"); // should print White
-    console.log(test);
+    Mouse = { name: "", price: 0 }; // Mouse object
+    let words = line.split(" "); // split line into words
+
+    for (let i = 0; i < words.length; i++) { // do each word
+        while (canSkipWord(words[i])) i++; // if it's the typical starter words, skip
+        if (miceNames.has(words[i])) { // if it's in the map
+            Mouse.name = miceNames.get(words[i++]); // insert the value as the name
+            while (i < words.length) { // 
+                if (!isNaN(words[i])) {
+                    Mouse.price = words[i];
+                    break;
+                }
+            }
+        }
+    }
+
+    // insert to db
+}
+
+function canSkipWord(word) {
+    switch (word) {
+        case "lf": return true;
+        case "sniper": return true;
+        case "snipers": return true;
+        case "sniping": return true;
+        default: return false;
+    }
 }
